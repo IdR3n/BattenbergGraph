232a032055992f6153cbe5a0544dd4567567c447
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 10:07:20 +0100

    removed react-bootstrap

--- batt-graph/src/Components/chart.jsx
+++ batt-graph/src/Components/chart.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import {Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Button } from "reactstrap";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
 function Chart() {
     return(
-        <button>wtf button</button>
+        <Button color="primary"> Reactstrap button</Button>
     );
 }
 
