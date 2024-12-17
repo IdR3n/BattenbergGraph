84aed7cdeaca42f6cbd8d4dcb6df0d3421b6b19f
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 13:04:33 +0100

    new

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -12,6 +12,7 @@ function App() {
    <div className="App">
     <NavStrap />
      <header className="App-header">
+
        </header>
      </div>
   );
--- batt-graph/src/Pagination/Adding.js
+++ batt-graph/src/Pagination/Adding.js
@@ -1,11 +1,9 @@
 import React from "react";
 import {Button} from "reactstrap";
-import NavStrap from "../Components/NavStrap";
 
 function Adding() {
     return(
         <div>
-            <NavStrap />
         <Button
           color="primary"
         >
