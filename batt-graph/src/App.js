2049267d189b14acc80b9a30a50209b9fb18e34c
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 14:40:30 +0100

    yes I know...

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //import logo from './logo.svg';
 //import Chart from './Components/chart.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavStrap from './Components/NavbarFIle/NavStrap.js';
+import Adding from './Pagination/Adding.js';
+import Graphing from './Pagination/Graphing.js';
 <link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
 
 function App() {
   return (
-   <div className="App">
-    <div className='NavStrap'>
+    <Router>
       <NavStrap />
-    </div>
-     <header className="App-header">
-       </header>
-     </div>
+      <Routes>
+        <Route path='./Pagination/Adding.js' element={<Adding />}/>
+        <Route path='./Pagination/Graphing.js' element={<Graphing />} />
+      </Routes>
+    </Router>
   );
- 
 }
 
 export default App;
--- batt-graph/src/Components/NavbarFIle/NavStrap.js
+++ batt-graph/src/Components/NavbarFIle/NavStrap.js
@@ -1,42 +1,21 @@
-import React, {useState} from "react";
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav,NavItem, NavLink, UncontrolledDropdown, DropdownToggle,DropdownMenu, DropdownItem,NavbarText,} from 'reactstrap';
+import React from "react";
+import {Link} from "react-router-dom";
+import {Navbar, NavbarBrand, Nav,NavItem, NavLink} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function NavStrap(args) {
-    const [isOpen, setIsOpen] = useState(false);
-        
-          const toggle = () => setIsOpen(!isOpen);
+import Adding from "../../Pagination/Adding";
+function NavStrap() {
     return (
-          <div>
-              <Navbar {...args}>
-                <NavbarBrand href="/">Battenberg Grapher</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                  <Nav className="me-auto" navbar>
-                    <NavItem>
-                      <NavLink href="/components/">Components</NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink href="https://github.com/IdR3n/BattenbergGraph/tree/dev">
-                        GitHub
-                      </NavLink>
-                    </NavItem>
-                    <UncontrolledDropdown nav inNavbar>
-                      <DropdownToggle nav caret>
-                        Options
-                      </DropdownToggle>
-                      <DropdownMenu right>
-                        <DropdownItem href="./Pagination/Adding.js">Add API
-                        </DropdownItem>
-                        <DropdownItem href="./Pagination/Graphing.js">Graphing</DropdownItem>
-                        <DropdownItem divider />
-                        <DropdownItem href="http://192.168.10.107:3000">Reset</DropdownItem>
-                      </DropdownMenu>
-                    </UncontrolledDropdown>
-                  </Nav>
-                  <NavbarText>Fondation Battenberg 2024-2025 published</NavbarText>
-                </Collapse>
-              </Navbar>
-            </div>
+          <Navbar color="danger" light expend="md">
+            <NavbarBrand href="http://192.168.10.107:3000">Battenberg Grapher</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink tag={Link} to="../../Pagination/Adding.js">Adding API</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="../../Pagination/Graphing.js">Graphing</NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
         
     );
 };
