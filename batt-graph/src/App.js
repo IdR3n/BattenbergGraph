ab563b06751530920cbaf37a91984b28ec7cc376
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 09:16:11 +0100

    added reactstrap, bootstrap, react-bootstrap

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -1,27 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import Chart from './Components/chart.jsx';
+//import Chart from './Components/chart.jsx';
+import Navibar from './Components/navbar.js';
 import 'bootstrap/dist/css/bootstrap.css';
+
+<link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
+
 function App() {
   return (
-   <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Chart></Chart>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navibar/>
   );
+ 
 }
 
 export default App;
+
+//  <div className="App">
+//    <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <Chart></Chart>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
