32793ffbbde23c6cd61b8975d0d291665fff9945
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 16:43:15 +0100

    progress

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import './App.css';
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import logo from './logo.svg';
-//import Chart from './Components/chart.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavStrap from './Components/NavbarFIle/NavStrap.js';
 import Adding from './Pagination/Adding.js';
 import Graphing from './Pagination/Graphing.js';
-<link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <NavStrap />
-      <Routes>
-        <Route path='./Pagination/Adding.js' element={<Adding />}/>
-        <Route path='./Pagination/Graphing.js' element={<Graphing />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <div className='Navbars'>
+          <NavStrap />
+        </div>
+        <div className='Carouselles'>
+          {/*L'autre choses qui devrait visible dans le page*/}
+        <Adding />
+        <Graphing />
+        
+        </div>
+      </header>
+    </div>
   );
 }
 
--- batt-graph/src/Components/NavbarFIle/NavStrap.js
+++ batt-graph/src/Components/NavbarFIle/NavStrap.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import './NavStrap.css';
 import {Link} from "react-router-dom";
-import {Navbar, NavbarBrand, Nav,NavItem, NavLink} from 'reactstrap';
+import {Navbar, NavbarBrand, Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Adding from "../../Pagination/Adding";
 function NavStrap() {
-    return (
-          <Navbar color="danger" light expend="md">
-            <NavbarBrand href="http://192.168.10.107:3000">Battenberg Grapher</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={Link} to="../../Pagination/Adding.js">Adding API</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} to="../../Pagination/Graphing.js">Graphing</NavLink>
-              </NavItem>
-            </Nav>
-          </Navbar>
-        
-    );
-};
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Sidebar'ın açılıp kapanmasını kontrol eden fonksiyon
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+     <Navbar className="my-2" color="danger" expand="md">
+        <Button color="light" onClick={toggleSidebar} style={{zIndex: 2, position: 'relative', marginRight: '15px'}}>☰</Button>
+        <NavbarBrand href="/" style={{
+          zIndex: 1,
+          color: 'white'
+        }}>          
+          Battenberg Grapher
+        </NavbarBrand>
+      </Navbar>
+      {/* Sidebar menü */}
+      <div className={`sidebar ${isOpen ? 'open' : ''}`} style={{zIndex: 1}}><br />
+        <a href="/">Page D'acceuil</a>
+        <a href="/">Add API</a>
+        <a href="/">Graphing</a>
+      </div>
+    </div>
+  );
+}
+
 export default NavStrap;
