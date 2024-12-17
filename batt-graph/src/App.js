8b031f45d411240c0432065f1402002375dd286c
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-17 10:53:43 +0100

    return to original

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -1,32 +1,33 @@
+import React from 'react';
 import './App.css';
-import Chart from './Components/chart.jsx';
+import logo from './logo.svg';
+//import Chart from './Components/chart.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 
 <link rel="stylesheet" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
 
 function App() {
   return (
-    <Chart/>
+   <div className="App">
+     <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+         <p>
+           Edit <code>src/App.js</code> and save to reload.
+         </p>
+         <a
+           className="App-link"
+           href="https: reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
   );
  
 }
 
 export default App;
 
-//  <div className="App">
-//    <header className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <Chart></Chart>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
+   
