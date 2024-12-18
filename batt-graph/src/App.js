b89df3ca9653df3be00e4980b9bb76031a4a333e
bidrIdr3n <bidrisarikaya@gmail.com>
2024-12-18 11:09:30 +0100

    All is wokring

--- batt-graph/src/App.js
+++ batt-graph/src/App.js
@@ -5,6 +5,7 @@ import NavStrap from './Components/NavbarFIle/NavStrap.js';
 import Adding from './Pagination/Adding.js';
 import Graphing from './Pagination/Graphing.js';
 import Documentation from './Pagination/Documentation.jsx';
+import HomePage from './Pagination/HomePage.js';
 import './App.css';
 
 function App() {
@@ -14,16 +15,11 @@ function App() {
       <header className="App-header">
         <NavStrap />
         <Routes>
+            <Route path="/" element={< HomePage/>} />
             <Route path="/adding" element={< Adding/>} />
             <Route path="/graphing" element={< Graphing/>} />
             <Route path="/documentation" element={< Documentation/>} />
         </Routes>
-          
-        <div className='bodyPlace'>
-          {/*L'autre choses qui devrait visible dans le page*/}
-          <h2 className='bodyPlace'>Bienvenue à Battenberg Grapher!</h2>
-          <p className='bodyPlace'>Ici vous pouvez observer les services que vous recevrez de Nagios Core avec des graphiques en direct(live graphing),<br /> Si vous êtes curieux comment cela fonctionne ou comment le faire, vous pouvez consulter la page Documentation dans le barre latéralle.</p>
-        </div>
         </header>
     </div>
     </Router>
---
+++ batt-graph/src/Pagination/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+    return(
+        <div className='bodyPlace'>
+          {/*L'autre choses qui devrait visible dans le page*/}
+          <h2 className='bodyPlace'>Bienvenue à Battenberg Grapher!</h2>
+          <p className='bodyPlace'>Ici vous pouvez observer les services que vous recevrez de Nagios Core avec des graphiques en direct(live graphing),<br /> Si vous êtes curieux comment cela fonctionne ou comment le faire, vous pouvez consulter la page Documentation dans le barre latéralle.</p>
+        </div>
+
+    );
+}
+
+export default HomePage;
